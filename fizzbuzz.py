ae011deb6103ab6124c33bf3a014d01831635aaf
vivosmith <livovivo@gmail.com>
2017-05-03 08:45:55 -0700

    FizzBuzz
    My Implementation of the age old fizzbuzz problem in Python.

---
+++ fizzbuzz.py
@@ -0,0 +1,9 @@
+hundred=0
+while(hundred!=100):
+      hundred=hundred+1
+      if hundred%3==0 :
+         print("fizz ",hundred)
+      if hundred%5==0:
+         print("buzz ",hundred)
+      if hundred%3==0 and hundred%5==0:
+         print("Fizz buzz ",hundred)
